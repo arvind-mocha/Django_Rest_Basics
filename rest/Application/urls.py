172b36692113fdfe1f8ce76538ba21a8ed221ce7
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-05-11 15:02:27 +0530

    Initial commit;

--- rest/Application/urls.py
+++ rest/Application/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView
+from .views import ArticleAPIView, ArticleDetails
 
 urlpatterns = [
     # path('article/', article_list, name='article'),
+    # path('detail/<str:pk>/', article_detail, name='article_detail'),
     path('article/', ArticleAPIView.as_view(), name='article'),
-    path('detail/<str:pk>/', article_detail, name='article_detail'),
+    path('detail/<str:id>/',ArticleDetails.as_view(), name='detail')
 ]
--- rest/Application/views.py
+++ rest/Application/views.py
@@ -25,45 +25,72 @@ class ArticleAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Function Based rest_framework views
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles,many=True)
-        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = ArticleSerializer(data=request.data)
+class ArticleDetails(APIView):
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET','PUT','DELETE'])
-def article_detail(request,pk):
-    try:
-        article = Article.objects.get(pk=pk)
-    except:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+    def get_object(self,id):
+        return Article.objects.get(id=id)
     
-    elif request.method == 'PUT':
+    def get(self,request,id):
+        try:
+            article = self.get_object(id)
+            serializer = ArticleSerializer(article)
+            return Response(serializer.data)
+        except:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self,request,id):
+        article = self.get_object(id)
         serializer = ArticleSerializer(article, data=request.data)
-
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
+
+    def delete(self,request,id):
+        article = self.get_object(id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Function Based rest_framework views
+
+# @api_view(['GET', 'POST'])
+# def article_list(request):
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles,many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = ArticleSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def article_detail(request,pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
+#     except:
+#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+    
+#     elif request.method == 'PUT':
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == 'DELETE':
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Normal Django Views
 
