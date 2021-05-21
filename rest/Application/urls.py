a058a9850684612ff424dd8e0eaa0d22a76644df
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-05-21 17:25:52 +0530

    Initial commit

--- rest/Application/urls.py
+++ rest/Application/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ArticleAPIView, ArticleDetails
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     # path('article/', article_list, name='article'),
     # path('detail/<str:pk>/', article_detail, name='article_detail'),
     path('article/', ArticleAPIView.as_view(), name='article'),
-    path('detail/<str:id>/',ArticleDetails.as_view(), name='detail')
+    path('detail/<str:id>/',ArticleDetails.as_view(), name='detail'),
+    path('generic/article/<str:id>', GenericAPIView.as_view(), name='generic-article'),
 ]
--- rest/Application/views.py
+++ rest/Application/views.py
@@ -8,7 +8,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics, mixins
 # Create your views here.
+
+#Generic Class Based Api
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin): # these mixins represents the api request we are permitted to perform 
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id' # Primary Key
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self,request):
+        return self.create(request)
+
+    def put(self,request,id = None):
+        return self.update(request,id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 # Class based rest_framework view
 
